a8ed2a6361326abb4e929f2fd181cc6c773c54e3
serfinetracy <tracyserfine@gmail.com>
2022-02-08 10:11:05 +0300

    Tu-Link Up

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const loginform = document.querySelector("#login");
-    const signUpForm = document.querySelector("signUp");
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
 
-    document.querySelector("#linkSignUp").addEventListener("click", (event) => {
-        event.preventDefault();
-        loginForm,classList,add("form--hidden");
-        signUpForm,classList,remove("form--hidden");
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const signupForm = document.querySelector("#SignUp");
+
+    document.querySelector("#linkSignUp").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        signupForm.classList.remove("form--hidden");
     });
 
-    document.querySelector("#linkLogin").addEventListener("click", (event) => {
-        event.preventDefault();
-        loginForm,classList,remove("form--hidden");
-        signUpForm,classList,add("form--hidden");
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        signupForm.classList.add("form--hidden");
+    });
+
+    loginForm.addEventListener("Continue", e => {
+        e.preventDefault();
+
+        // Perform your AJAX/Fetch login
+
+        setFormMessage(loginForm, "Error", "Invalid username/password");
+    });
+
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 4 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
     });
 });
